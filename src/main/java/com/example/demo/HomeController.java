9f75f1f0907d433a6d149015a48c113e692ea843
Terry Farria <tfjteachus@gmail.com>
2018-04-20 16:10:29 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,56 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @Autowired
+    PetRepository  petRepo;
+
+    @RequestMapping("/")
+    public String showPet(Model model){
+        model.addAttribute("petList", petRepo.findAll());
+        return "showpet";
+    }
+
+    @RequestMapping("/add")
+    public String addPet(Model model){
+        model.addAttribute("pet", new Pet());
+        return "addpet";
+    }
+
+    @RequestMapping("/savepet")
+    public String savePet(@ModelAttribute("pet") Pet toSave, BindingResult result){
+        if(result.hasErrors()){
+            return "addpet";
+        }
+        petRepo.save(toSave);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/changestatus/{id}")
+    public String foundPet(@PathVariable("id") long id){
+        Pet thisPet = petRepo.findById(id).get();
+
+        thisPet.setFound(!thisPet.isFound());
+
+        petRepo.save(thisPet);
+        return "redirect:/";
+    }
+    @RequestMapping("/update/{id}")
+    public String updatePet(@PathVariable("id") long id, Model model){
+        model.addAttribute("pet", petRepo.findById(id));
+        return "addpet";
+    }
+    @RequestMapping("/delete/{id}")
+    public String deletePet(@PathVariable("id") long id){
+        petRepo.deleteById(id);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/example/demo/Pet.java
@@ -0,0 +1,87 @@
+package com.example.demo;
+import javax.persistence.Entity;
+
+
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Size(min = 1)
+    private String type;
+
+    @NotNull
+    @Size(min = 1)
+    private String name;
+
+    @NotNull
+    @Size(min = 1)
+    private String description;
+
+    private String image;
+
+    private boolean found;
+
+    public Pet() {
+        this.found = false;
+    }
+
+    public boolean isFound(){
+        return found;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setFound(boolean found) {
+        this.found = found;
+    }
+}
